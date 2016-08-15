a51aaa1a462e709f3e820373113a18a3718013c7
Lex van Roon <lvanroon@bol.com>
2016-08-15 12:39:57 +0200

    Make several parameters configurable

--- go-paste.go
+++ go-paste.go
@@ -4,14 +4,27 @@ import (
 	"flag"
 	"github.com/r3boot/go-paste/lib"
 	"github.com/r3boot/rlib/logger"
-	_ "time"
 )
 
 // Default values for commandline paramenters
 const D_DEBUG bool = false
+const D_BINDIP string = "0.0.0.0"
+const D_BINDPORT int = 8080
+const D_REDISADDR string = "localhost:6379"
+const D_REDISPASS string = ""
+const D_REDISDB int64 = 0
+const D_TEMPLATE string = "/usr/share/go-paste.html"
+const D_TITLE string = "Pastebin"
 
 // Commandline paramenters
 var debug = flag.Bool("D", D_DEBUG, "Enable debug output")
+var bindip = flag.String("l", D_BINDIP, "IP to listen on")
+var bindport = flag.Int("p", D_BINDPORT, "Port to listen on")
+var template = flag.String("t", D_TEMPLATE, "Template to use")
+var redisaddr = flag.String("ra", D_REDISADDR, "Address on which redis is running")
+var redispass = flag.String("rp", D_REDISPASS, "Password used to connect to redis")
+var redisdb = flag.Int64("rd", D_REDISDB, "Redis DB to connect to")
+var title = flag.String("T", D_TITLE, "Title to use for the webapp")
 
 // Logging framework
 var Log logger.Log
@@ -27,13 +40,13 @@ func init() {
 	Log.Debug("Logging initialized")
 
 	lib.Setup(Log, &lib.AppConfig{
-		BindIp:      "0.0.0.0",
-		BindPort:    8080,
-		RedisAddr:   "redis.service.local:6379",
-		RedisPasswd: "",
-		RedisDB:     0,
-		Title:       "as65342 paste",
-		Template:    "./templates/index.html",
+		BindIp:      *bindip,
+		BindPort:    *bindport,
+		RedisAddr:   *redisaddr,
+		RedisPasswd: *redispass,
+		RedisDB:     *redisdb,
+		Title:       *title,
+		Template:    *template,
 	})
 }
 
